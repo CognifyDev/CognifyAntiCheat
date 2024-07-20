8caab10f5b8a250abf753ab4e9023ef226508a4a
Night-GUA <xiguaos@163.com>
2024-07-20 21:19:50 +0800

    add two GameStatesAPI

--- CognifyAntiCheat/States/GameStates.cs
+++ CognifyAntiCheat/States/GameStates.cs
@@ -1,4 +1,5 @@
 using InnerNet;
+using AmongUs.GameOptions;
 
 namespace CognifyAntiCheat.States;
 
@@ -30,4 +31,17 @@ public static class GameStates
     public static bool IsVoting => IsMeeting &&
                                    MeetingHud.Instance.state is MeetingHud.VoteStates.Voted
                                        or MeetingHud.VoteStates.NotVoted;
+    
+    /// <summary>
+    ///     是否是躲猫猫模式
+    /// </summary>
+    public static bool isHideNSeek => GameOptionsManager.Instance.CurrentGameOptions.GameMode == GameModes.HideNSeek;
+    
+    /// <summary>
+    ///     获取真实名
+    /// </summary>
+    public static string GetRealName(this PlayerControl player, bool isMeeting = false)
+    {
+        return isMeeting ? player?.Data?.PlayerName : player?.name;
+    }
 }
